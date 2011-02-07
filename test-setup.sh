0d5cc9bbac66b45e82b42f5a6af892ed40b11f72
Robert Surton <burgess@cs.cornell.edu>
2011-02-07 17:21:56 -0500

    Changed network for unit tests, tweaked setup script.

--- test-setup.sh
+++ test-setup.sh
@@ -1,6 +1,6 @@
-echo 0 | tee /proc/sys/net/ipv4/conf/*/send-redirects > /dev/null
+echo 0 | tee /proc/sys/net/ipv4/conf/*/send_redirects > /dev/null
 echo 1 | tee /proc/sys/net/ipv4/ip_forward > /dev/null
-openvpn --mktun --dev tcpr-test --dev-type tun --user `whoami`
+openvpn --mktun --dev tcpr-test --dev-type tun --user $1
 ip link set tcpr-test up
-ip addr add 10.0.0.1/24 dev tcpr-test
-iptables -A FORWARD -s 10.0.0.1/24 -j NFQUEUE
+ip addr add 10.10.10.1/24 dev tcpr-test
+iptables -A FORWARD -s 10.10.10.1/24 -j NFQUEUE
