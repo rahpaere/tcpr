0260ff77d4f914db122a8e673dab7bd3d2f9cd11
Robert Surton <burgess@cs.cornell.edu>
2011-08-28 22:40:45 -0400

    Replace tcpr-debug with tcpr-control.

--- src/control.c
+++ src/control.c
@@ -24,6 +24,14 @@ struct state {
 	const char *peer_host;
 	const char *peer_port;
 	const char *port;
+	const char *save_file;
+	const char *recovery_file;
+	int input_bytes;
+	int output_bytes;
+	int wait;
+	int destroy;
+	struct sockaddr_in peer_address;
+	struct sockaddr_in address;
 	struct tcpr_connection tcpr;
 };
 
@@ -31,13 +39,20 @@ static void print_help_and_exit(const char *program)
 {
 	fprintf(stderr, "Usage: %s [OPTIONS]\n", program);
 	fprintf(stderr, "\n");
-	fprintf(stderr, "Print a human-readable representation of TCPR "
-		"connection state.\n");
+	fprintf(stderr, "Manipulate and display TCPR connection state.\n");
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Options:\n");
 	fprintf(stderr, "  -a PORT  The application is bound to PORT.\n");
 	fprintf(stderr, "  -h HOST  The peer is bound to HOST.\n");
 	fprintf(stderr, "  -p PORT  The peer is bound to PORT.\n");
+	fprintf(stderr, "  -S FILE  Save the connection state into FILE.\n");
+	fprintf(stderr, "  -R FILE  Recover the connection state from FILE.\n");
+	fprintf(stderr, "  -I NUM   Acknowledge NUM bytes of input;\n"
+		"           if NUM is negative, shut down input.\n");
+	fprintf(stderr, "  -O NUM   Checkpoint NUM bytes of output;\n"
+		"           if NUM is negative, shut down output.\n");
+	fprintf(stderr, "  -W       Wait until the connection is done.\n");
+	fprintf(stderr, "  -D       Destroy the connection state.\n");
 	fprintf(stderr, "  -?       Print this help message and exit.\n");
 	exit(EXIT_FAILURE);
 }
@@ -49,17 +64,41 @@ static void handle_options(struct state *s, int argc, char **argv)
 	s->peer_host = "127.0.0.1";
 	s->peer_port = "9999";
 	s->port = "8888";
+	s->save_file = NULL;
+	s->recovery_file = NULL;
+	s->input_bytes = 0;
+	s->output_bytes = 0;
+	s->wait = 0;
+	s->destroy = 0;
 
-	while ((o = getopt(argc, argv, "h:p:P?")) != -1)
+	while ((o = getopt(argc, argv, "a:h:p:S:R:I:O:WD?")) != -1)
 		switch (o) {
+		case 'a':
+			s->port = optarg;
+			break;
 		case 'h':
 			s->peer_host = optarg;
 			break;
 		case 'p':
 			s->peer_port = optarg;
 			break;
-		case 'P':
-			s->port = optarg;
+		case 'S':
+			s->save_file = optarg;
+			break;
+		case 'R':
+			s->recovery_file = optarg;
+			break;
+		case 'I':
+			s->input_bytes = atoi(optarg);
+			break;
+		case 'O':
+			s->output_bytes = atoi(optarg);
+			break;
+		case 'W':
+			s->wait = 1;
+			break;
+		case 'D':
+			s->destroy = 1;
 			break;
 		default:
 			print_help_and_exit(argv[0]);
@@ -69,6 +108,7 @@ static void handle_options(struct state *s, int argc, char **argv)
 static void setup_state(struct state *s)
 {
 	int err;
+	int flags = 0;
 	struct addrinfo *ai;
 	struct addrinfo *peer_ai;
 	struct addrinfo hints;
@@ -83,22 +123,63 @@ static void setup_state(struct state *s)
 		fprintf(stderr, "Resolving peer: %s\n", gai_strerror(err));
 		exit(EXIT_FAILURE);
 	}
+	memcpy(&s->peer_address, peer_ai->ai_addr, sizeof(s->peer_address));
+	freeaddrinfo(peer_ai);
 
 	err = getaddrinfo(NULL, s->port, &hints, &ai);
 	if (err) {
 		fprintf(stderr, "Resolving port: %s\n", gai_strerror(err));
 		exit(EXIT_FAILURE);
 	}
+	memcpy(&s->address, ai->ai_addr, sizeof(s->address));
+	freeaddrinfo(ai);
 
+	if (s->recovery_file)
+		flags |= TCPR_CONNECTION_CREATE;
 	if (tcpr_setup_connection
-	    (&s->tcpr, (struct sockaddr_in *)peer_ai->ai_addr,
-	     ((struct sockaddr_in *)ai->ai_addr)->sin_port, 0) < 0) {
+	    (&s->tcpr, &s->peer_address, s->address.sin_port, flags) < 0) {
 		perror("Opening state");
 		exit(EXIT_FAILURE);
 	}
+}
 
-	freeaddrinfo(ai);
-	freeaddrinfo(peer_ai);
+static void update_state(struct state *s)
+{
+	int fd;
+
+	if (s->recovery_file) {
+		fd = open(s->recovery_file, O_RDONLY);
+		if (fd < 0) {
+			perror("Opening recovery file");
+			exit(EXIT_FAILURE);
+		}
+		if (read
+		    (fd, &s->tcpr.state->saved,
+		     sizeof(s->tcpr.state->saved)) !=
+		    sizeof(s->tcpr.state->saved)) {
+			perror("Recovering");
+			exit(EXIT_FAILURE);
+		}
+		if (close(fd) < 0) {
+			perror("Closing recovery file");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (s->output_bytes < 0)
+		tcpr_done_writing(&s->tcpr);
+	else if (s->output_bytes > 0)
+		tcpr_advance(&s->tcpr, s->output_bytes);
+	if (s->input_bytes < 0)
+		tcpr_done_reading(&s->tcpr);
+	else if (s->input_bytes > 0)
+		tcpr_consume(&s->tcpr, s->input_bytes);
+}
+
+static void wait_until_done(struct state *s)
+{
+	while (!s->tcpr.state->done)
+		sleep(1);
 }
 
 static void print_state(struct state *s)
@@ -128,16 +209,39 @@ static void print_state(struct state *s)
 		       ntohl(s->tcpr.state->peer.fin));
 	printf("Peer WIN: %" PRIu16 "\n", ntohs(s->tcpr.state->peer.win));
 	if (s->tcpr.state->saved.done_reading)
-		printf("Done reading.\n");
+		printf("The application is done reading.\n");
 	if (s->tcpr.state->saved.done_writing)
-		printf("Done writing.\n");
+		printf("The application is done writing.\n");
 	if (s->tcpr.state->done)
-		printf("Done.\n");
+		printf("The connection is finished.\n");
 }
 
 static void teardown_state(struct state *s)
 {
+	int fd;
+
+	if (s->save_file) {
+		fd = creat(s->save_file, 0600);
+		if (fd < 0) {
+			perror("Opening save file");
+			exit(EXIT_FAILURE);
+		}
+		if (write
+		    (fd, &s->tcpr.state->saved,
+		     sizeof(s->tcpr.state->saved)) !=
+		    sizeof(s->tcpr.state->saved)) {
+			perror("Saving");
+			exit(EXIT_FAILURE);
+		}
+		if (close(fd) < 0) {
+			perror("Closing save file");
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	tcpr_teardown_connection(&s->tcpr);
+	if (s->destroy)
+		tcpr_destroy_connection(&s->peer_address, s->address.sin_port);
 }
 
 int main(int argc, char **argv)
@@ -147,6 +251,9 @@ int main(int argc, char **argv)
 	handle_options(&s, argc, argv);
 
 	setup_state(&s);
+	update_state(&s);
+	if (s.wait)
+		wait_until_done(&s);
 	print_state(&s);
 	teardown_state(&s);
 
