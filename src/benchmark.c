2635a6722162d9d52fb0e0beb7242274fef2f26f
Robert Surton <burgess@cs.cornell.edu>
2011-09-30 14:05:31 -0400

    Add peering ability to tcpr-benchmark, simplify output, increase number of connections.

--- src/benchmark.c
+++ src/benchmark.c
@@ -3,14 +3,16 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
 
-#define NUM_CONNECTIONS 32
+#define NUM_CONNECTIONS 256
 
 static const char *internal_host = "10.0.1.1";
 static const char *peer_host = "10.0.0.2";
-static const char *peer_port = "echo";
+static const char *peer_port = "9999";
+static int running_peer;
+static int listening_socket;
 
 struct connection {
 	int sock;
@@ -29,6 +31,7 @@ static void print_help_and_exit(const char *program)
 	fprintf(stderr, "  -i HOST  Internally, the application is bound to HOST.\n");
 	fprintf(stderr, "  -h HOST  The peer is bound to HOST.\n");
 	fprintf(stderr, "  -p PORT  The peer is bound to PORT.\n");
+	fprintf(stderr, "  -P       Run as the peer.\n");
 	fprintf(stderr, "  -?       Print this help message and exit.\n");
 	exit(EXIT_FAILURE);
 }
@@ -36,7 +39,7 @@ static void print_help_and_exit(const char *program)
 static void handle_options(int argc, char **argv)
 {
 	for (;;)
-		switch (getopt(argc, argv, "i:h:p:?")) {
+		switch (getopt(argc, argv, "i:h:p:P?")) {
 		case 'i':
 			internal_host = optarg;
 			break;
@@ -46,6 +49,9 @@ static void handle_options(int argc, char **argv)
 		case 'p':
 			peer_port = optarg;
 			break;
+		case 'P':
+			running_peer = 1;
+			break;
 		case -1:
 			return;
 		default:
@@ -53,7 +59,81 @@ static void handle_options(int argc, char **argv)
 		}
 }
 
-static void open_and_fail(struct connection *c)
+static void open_listening_socket(void)
+{
+	int err;
+	int s;
+	int yes = 1;
+	struct addrinfo *ai;
+	struct addrinfo hints;
+
+	s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s < 0) {
+		perror("Creating socket");
+		exit(EXIT_FAILURE);
+	}
+
+	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
+		perror("Setting SO_REUSEADDR");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	err = getaddrinfo(NULL, peer_port, &hints, &ai);
+	if (err) {
+		fprintf(stderr, "Resolving bind address: %s\n",
+				gai_strerror(err));
+		exit(EXIT_FAILURE);
+	}
+	if (bind(s, ai->ai_addr, ai->ai_addrlen) < 0) {
+		perror("Binding");
+		exit(EXIT_FAILURE);
+	}
+	freeaddrinfo(ai);
+
+	if (listen(s, NUM_CONNECTIONS) < 0) {
+		perror("Listening");
+		exit(EXIT_FAILURE);
+	}
+
+	listening_socket = s;
+}
+
+static void accept_connection(struct connection *c)
+{
+	c->sock = accept(listening_socket, NULL, NULL);
+	if (c->sock < 0) {
+		perror("Accepting");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void reap_connection(struct connection *c)
+{
+	char buf[64];
+	int err;
+
+	do {
+		err = read(c->sock, buf, sizeof(buf));
+	} while (err > 0);
+	if (err < 0) {
+		perror("Consuming input");
+		exit(EXIT_FAILURE);
+	}
+	close(c->sock);
+}
+
+static void close_listening_socket(void)
+{
+	close(listening_socket);
+}
+
+static void open_benchmark_socket(struct connection *c)
 {
 	int err;
 	int s;
@@ -109,7 +189,10 @@ static void open_and_fail(struct connection *c)
 
 	addrlen = sizeof(c->addr);
 	getsockname(s, (struct sockaddr *)&c->addr, &addrlen);
+}
 
+static void fail(struct connection *c)
+{
 	close(c->sock);
 }
 
@@ -139,15 +222,12 @@ static void recover(struct connection *c)
 		exit(EXIT_FAILURE);
 	}
 
-	c->sock = s;
-}
-
-static void resetup(struct connection *c)
-{
 	if (tcpr_setup_connection(&c->tcpr, c->peer_addr.sin_addr.s_addr, c->peer_addr.sin_port, c->addr.sin_port, 0) < 0) {
 		perror("Recovering, setting up TCPR");
 		exit(EXIT_FAILURE);
 	}
+
+	c->sock = s;
 }
 
 static void teardown(struct connection *c)
@@ -156,6 +236,7 @@ static void teardown(struct connection *c)
 	int err;
 
 	tcpr_close(&c->tcpr);
+	tcpr_teardown_connection(&c->tcpr);
 
 	shutdown(c->sock, SHUT_WR);
 	do {
@@ -167,45 +248,46 @@ static void teardown(struct connection *c)
 	}
 
 	close(c->sock);
-	tcpr_wait(&c->tcpr);
-	tcpr_teardown_connection(&c->tcpr);
 }
 
 int main(int argc, char **argv)
 {
 	struct timespec start;
 	struct timespec end;
-	double recover_time;
-	double resetup_time;
+	double t;
 	int i;
 
 	handle_options(argc, argv);
 
-	for (i = 0; i < NUM_CONNECTIONS; i++)
-		open_and_fail(&connections[i]);
+	if (running_peer) {
+		open_listening_socket();
+		for (i = 0; i < NUM_CONNECTIONS; i++)
+			accept_connection(&connections[i]);
+		close_listening_socket();
+		for (i = 0; i < NUM_CONNECTIONS; i++)
+			reap_connection(&connections[i]);
+		return EXIT_SUCCESS;
+	}
 
-	sleep(1);
+	for (i = 0; i < NUM_CONNECTIONS; i++)
+		open_benchmark_socket(&connections[i]);
+
+	//sleep(30);
+
+	for (i = 0; i < NUM_CONNECTIONS; i++)
+		fail(&connections[i]);
+
+	sleep(30);
 
 	clock_gettime(CLOCK_REALTIME, &start);
 	for (i = 0; i < NUM_CONNECTIONS; i++)
 		recover(&connections[i]);
 	clock_gettime(CLOCK_REALTIME, &end);
 
-	recover_time = (double)end.tv_sec + (double)end.tv_nsec / 1e9;
-	recover_time -= (double)start.tv_sec + (double)start.tv_nsec / 1e9;
-	recover_time /= (double)NUM_CONNECTIONS;
-
-	clock_gettime(CLOCK_REALTIME, &start);
-	for (i = 0; i < NUM_CONNECTIONS; i++)
-		resetup(&connections[i]);
-	clock_gettime(CLOCK_REALTIME, &end);
-
-	resetup_time = (double)end.tv_sec + (double)end.tv_nsec / 1e9;
-	resetup_time -= (double)start.tv_sec + (double)start.tv_nsec / 1e9;
-	resetup_time /= (double)NUM_CONNECTIONS;
-
-	printf("Recover,Resetup\n");
-	printf("%lf,%lf\n", recover_time, resetup_time);
+	t = (double)end.tv_sec + (double)end.tv_nsec / 1e9;
+	t -= (double)start.tv_sec + (double)start.tv_nsec / 1e9;
+	t /= (double)NUM_CONNECTIONS;
+	printf("%lf\n", t);
 
 	for (i = 0; i < NUM_CONNECTIONS; i++)
 		teardown(&connections[i]);
