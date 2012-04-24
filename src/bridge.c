95a69f893bc9bf179b4232f69e81edaa0058d812
Robert Surton <burgess@cs.cornell.edu>
2012-04-24 15:12:57 -0400

    Rewrite bridge to use UDP interface.

--- src/bridge.c
+++ src/bridge.c
@@ -1,4 +1,4 @@
-#include <tcpr/application.h>
+#include <tcpr/types.h>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -11,10 +11,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 struct client {
 	int sock;
-	struct tcpr_connection tcpr;
+	struct tcpr_ip4 state;
 	struct sockaddr_in address;
 	struct sockaddr_in peer_address;
 	char buffer[512];
@@ -22,16 +23,17 @@ struct client {
 	int shut;
 };
 
-static const char state_path_format[] = "/var/tmp/tcpr-bridge-%s-%s.state";
+static const char state_path_format[] = "/var/tmp/tcpr-bridge-%s.state";
 
-static const char *host = "127.0.0.3";
-static const char *port = "8888";
+static char *tcpr_address;
+static char *bind_address;
 
-static int using_tcpr = 1;
 static int checkpointing = 1;
 static int recovering;
 
 static struct client *clients;
+
+static int tcpr_sock;
 
 static void print_help_and_exit(const char *program)
 {
@@ -40,44 +42,41 @@ static void print_help_and_exit(const char *program)
 	fprintf(stderr, "Bridge data across client connections, using TCPR.\n");
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "  -i HOST  "
-		"Internally, the application is bound to HOST.\n");
-	fprintf(stderr, "  -a PORT  The application is bound to PORT.\n");
-	fprintf(stderr, "  -C       Bypass checkpointed acknowledgments.\n");
-	fprintf(stderr, "  -T       Do not use TCPR.\n");
-	fprintf(stderr, "  -?       Print this help message and exit.\n");
+	fprintf(stderr, "  -b [HOST]:PORT  Bind to HOST:PORT.\n");
+	fprintf(stderr, "  -t [HOST]:PORT  Connect to TCPR at HOST:PORT.\n");
+	fprintf(stderr, "  -C              Bypass checkpointed acknowledgments.\n");
+	fprintf(stderr, "  -?              Print this help message and exit.\n");
 	exit(EXIT_FAILURE);
 }
 
 static void handle_options(int argc, char **argv)
 {
 	for (;;)
-		switch (getopt(argc, argv, "i:a:CT?")) {
-		case 'i':
-			host = optarg;
+		switch (getopt(argc, argv, "b:t:C?")) {
+		case 'b':
+			bind_address = optarg;
 			break;
-		case 'a':
-			port = optarg;
+		case 't':
+			tcpr_address = optarg;
 			break;
 		case 'C':
 			checkpointing = 0;
-			break;
-		case 'T':
-			using_tcpr = 0;
 			break;
 		case -1:
 			return;
 		default:
 			print_help_and_exit(argv[0]);
 		}
+	if (!tcpr_address || !bind_address)
+		print_help_and_exit(argv[0]);
 }
 
 static void setup(void)
 {
-	char path[sizeof(state_path_format) + strlen(host) + strlen(port)];
+	char path[sizeof(state_path_format) + strlen(bind_address)];
 	int fd;
 
-	sprintf(path, state_path_format, host, port);
+	sprintf(path, state_path_format, bind_address);
 	
 	fd = open(path, O_RDWR | O_CREAT | O_EXCL, 0600);
 	if (fd < 0) {
@@ -103,6 +102,8 @@ static void setup(void)
 
 static void setup_connections(void)
 {
+	char *host;
+	char *port;
 	int err;
 	int s;
 	int yes = 1;
@@ -119,6 +120,15 @@ static void setup_connections(void)
 	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
 		perror("Setting SO_REUSEADDR");
 		exit(EXIT_FAILURE);
+	}
+
+	port = strchr(bind_address, ':');
+	if (port) {
+		host = bind_address;
+		*port++ = '\0';
+	} else {
+		port = bind_address;
+		host = NULL;
 	}
 
 	memset(&hints, 0, sizeof(hints));
@@ -150,15 +160,6 @@ static void setup_connections(void)
 		}
 		addrlen = sizeof(clients[i].address);
 		getsockname(clients[i].sock, (struct sockaddr *)&clients[i].address, &addrlen);
-
-		if (using_tcpr) {
-			if (tcpr_setup_connection(&clients[i].tcpr, clients[i].peer_address.sin_addr.s_addr, clients[i].peer_address.sin_port, clients[i].address.sin_port, 0) < 0) {
-				perror("Opening TCPR state");
-				exit(EXIT_FAILURE);
-			}
-			if (!checkpointing)
-				tcpr_shutdown_input(&clients[i].tcpr);
-		}
 	}
 
 	close(s);
@@ -179,22 +180,6 @@ static void recover_connections(void)
 			perror("Setting SO_REUSEADDR");
 			exit(EXIT_FAILURE);
 		}
-
-		if (using_tcpr) {
-			if (tcpr_setup_connection(&clients[i].tcpr, clients[i].peer_address.sin_addr.s_addr, clients[i].peer_address.sin_port, clients[i].address.sin_port, 0) < 0) {
-				perror("Opening TCPR state");
-				exit(EXIT_FAILURE);
-			}
-			if (!checkpointing)
-				tcpr_shutdown_input(&clients[i].tcpr);
-			if (clients[i].shut) {
-				/* XXX do something */
-			}
-			if (clients[1 - i].shut) {
-				/* XXX do something */
-			}
-		}
-
 		if (bind(clients[i].sock, (struct sockaddr *)&clients[i].address,
 					sizeof(clients[i].address)) < 0) {
 			perror("Binding");
@@ -204,6 +189,66 @@ static void recover_connections(void)
 					sizeof(clients[i].peer_address)) < 0) {
 			perror("Connecting");
 			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+static void setup_tcpr(void)
+{
+	char *host;
+	char *port;
+	int err;
+	int s;
+	int i;
+	struct addrinfo *ai;
+	struct addrinfo hints;
+
+	s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (s < 0) {
+		perror("Creating socket");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+
+	port = strchr(tcpr_address, ':');
+	if (port) {
+		host = tcpr_address;
+		*port++ = '\0';
+	} else {
+		port = tcpr_address;
+		host = NULL;
+	}
+
+	err = getaddrinfo(host, port, &hints, &ai);
+	if (err) {
+		fprintf(stderr, "Resolving \"%s\": %s\n", tcpr_address, gai_strerror(err));
+		exit(EXIT_FAILURE);
+	}
+
+	if (connect(s, ai->ai_addr, ai->ai_addrlen) < 0) {
+		perror("Connecting to TCPR");
+		exit(EXIT_FAILURE);
+	}
+
+	tcpr_sock = s;
+
+	for (i = 0; i < 2; i++) {
+		clients[i].state.address = clients[i].address.sin_addr.s_addr;
+		clients[i].state.peer_address = clients[i].peer_address.sin_addr.s_addr;
+		clients[i].state.tcpr.hard.port = clients[i].address.sin_port;
+		clients[i].state.tcpr.hard.peer.port = clients[i].peer_address.sin_port;
+		clients[i].state.tcpr.port = 0;
+
+		send(tcpr_sock, &clients[i].state, sizeof(clients[i].state), 0);
+		recv(tcpr_sock, &clients[i].state, sizeof(clients[i].state), 0);
+
+		if (!checkpointing) {
+			clients[i].state.tcpr.hard.done_reading = 1;
+			send(tcpr_sock, &clients[i].state, sizeof(clients[i].state), 0);
 		}
 	}
 }
@@ -235,20 +280,22 @@ static void handle_events(void)
 			if (FD_ISSET(clients[i].sock, &rfds)) {
 				n = read(clients[i].sock, &clients[1 - i].buffer[clients[1 - i].size], sizeof(clients[1 - i].buffer) - clients[1 - i].size);
 				if (n > 0) {
-					if (using_tcpr && checkpointing)
-						tcpr_checkpoint_input(&clients[i].tcpr, n);
+					if (checkpointing) {
+						clients[i].state.tcpr.hard.ack = htonl(ntohl(clients[i].state.tcpr.hard.ack) + n);
+						send(tcpr_sock, &clients[i].state, sizeof(clients[i].state), 0);
+					}
 					clients[1 - i].size += n;
 				} else if (n < 0) {
 					perror("Reading");
 					exit(EXIT_FAILURE);
 				} else {
-					if (using_tcpr)
-						tcpr_shutdown_input(&clients[i].tcpr);
+					clients[i].state.tcpr.hard.done_reading = 1;
+					send(tcpr_sock, &clients[i].state, sizeof(clients[i].state), 0);
 					shutdown(clients[i].sock, SHUT_RD);
 					clients[i].shut = 1;
 					if (clients[1 - i].size == 0) {
-						if (using_tcpr)
-							tcpr_shutdown_output(&clients[1 - i].tcpr);
+						clients[1 - i].state.tcpr.hard.done_writing = 1;
+						send(tcpr_sock, &clients[1 - i].state, sizeof(clients[1 - i].state), 0);
 						shutdown(clients[1 - i].sock, SHUT_WR);
 					}
 				}
@@ -261,8 +308,8 @@ static void handle_events(void)
 					if (clients[i].size > 0) {
 						memmove(clients[i].buffer, &clients[i].buffer[n], clients[i].size);
 					} else if (clients[1 - i].shut) {
-						if (using_tcpr)
-							tcpr_shutdown_output(&clients[i].tcpr);
+						clients[i].state.tcpr.hard.done_writing = 1;
+						send(tcpr_sock, &clients[i].state, sizeof(clients[i].state), 0);
 						shutdown(clients[i].sock, SHUT_WR);
 					}
 				} else if (n < 0) {
@@ -276,21 +323,19 @@ static void handle_events(void)
 
 static void teardown(void)
 {
-	char path[sizeof(state_path_format) + strlen(host) + strlen(port)];
+	char path[sizeof(state_path_format) + strlen(bind_address)];
 	int i;
 
 	for (i = 0; i < 2; ++i) {
-		if (using_tcpr) {
-			tcpr_wait(&clients[i].tcpr);
-			tcpr_teardown_connection(&clients[i].tcpr);
-		}
 		if (close(clients[i].sock) < 0) {
 			perror("Closing");
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	sprintf(path, state_path_format, host, port);
+	close(tcpr_sock);
+
+	sprintf(path, state_path_format, bind_address);
 	unlink(path);
 
 	munmap(clients, 2 * sizeof(clients[0]));
@@ -304,6 +349,7 @@ int main(int argc, char **argv)
 		recover_connections();
 	else
 		setup_connections();
+	setup_tcpr();
 	handle_events();
 	teardown();
 	return EXIT_SUCCESS;
